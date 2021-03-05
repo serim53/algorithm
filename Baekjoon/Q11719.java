e4eee9277b8c3996acab0c0278022acbf61e7e8f
serim53 <tpfla9953@naver.com>
2021-03-05 20:10:12 +0900

    그대로 출력하기

---
+++ Baekjoon/Q11719.java
@@ -0,0 +1,31 @@
+package Baekjoon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q11719 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> list = new ArrayList<String>();
+		
+		while(sc.hasNext()) {
+			
+			String input = sc.nextLine();
+			
+			if(input == null)
+				break;
+			
+			list.add(input);
+			
+		}
+		
+		sc.close();
+		
+		for(int i=0;i<list.size();i++)
+			System.out.println(list.get(i));
+		
+	}
+
+}
