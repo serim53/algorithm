fd05ac10dcfe468bad3c77136c5b1952059f77a4
serim53 <tpfla9953@naver.com>
2021-03-06 22:59:23 +0900

    구구단

---
+++ Baekjoon/Q2739.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Q2739 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		sc.close();
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(N + " * " + i + " = " + N*i);
+		}
+	}
+
+}
