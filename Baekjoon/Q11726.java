0dd0b5308769cb6079d827dca2efc101b89c496b
serim53 <tpfla9953@naver.com>
2021-03-11 22:31:37 +0900

    DP/2×n 타일링

---
+++ Baekjoon/Q11726.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Q11726 {
+
+	public static void main(String args[]){
+		
+		Scanner sc = new Scanner(System.in);
+		
+        int n = sc.nextInt();
+
+   		int[] d = new int[n+1];
+
+    	d[0] = 1;
+    	d[1] = 1;
+
+    	if(n>=2){
+        	for(int i = 2; i <= n; i++){
+            	d[i] = (d[i-1] + d[i-2]) % 10007;
+        	}
+    	}
+    	System.out.println(d[n]);
+	}
+ }
--- Baekjoon/Q1463.java
+++ Baekjoon/Q1463.java
@@ -4,33 +4,43 @@ import java.util.Scanner;
 
 public class Q1463 {
 
+	static Integer[] dp;
+	
 	public static void main(String[] args) {
+
 
 		Scanner sc = new Scanner(System.in);
 		
-		int X = sc.nextInt();
+		int N = sc.nextInt();
 		
-		int num = 0;
+		dp = new Integer[N + 1];
 		
-		while(X!=1) {
-			
-			if(X%3==0) {
-				X = X/3;
-				num++;
-			}
-			else if(X%2==0) {
-				X = X/2;
-				num++;
-			}
-			else {
-				X = X-1;
-				num++;
-			}
-		}
+		dp[0] = dp[1] = 0;
 		
-		System.out.print(num);
-		
-		
-	}
+		System.out.print(recur(N));
 
-}
+
+
+	}
+	
+	static int recur(int N) {
+		
+		if (dp[N] == null) {
+			
+			if (N % 6 == 0) {
+				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+				}
+			else if (N % 3 == 0) {
+				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+				}
+			else if (N % 2 == 0) {
+				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+				}
+			else {
+				dp[N] = recur(N - 1) + 1;
+				}
+			}
+		return dp[N];
+		}
+
+	}
