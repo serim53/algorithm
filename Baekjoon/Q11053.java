eaf15fd829904d6de562e94b43c757f40fabe031
serim53 <tpfla9953@naver.com>
2021-03-17 21:42:48 +0900

    DP/가장 긴 증가하는 부분 수열

--- Baekjoon/Q11053.java
+++ Baekjoon/Q11053.java
@@ -4,33 +4,42 @@ import java.util.Scanner;
 
 public class Q11053 {
 
-	// 수정필요
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int[] A = new int[N];
-		// 현재 수(큰 수 발생시 변함)
-		int now = 0;
-		// 수열 안 원소의 개수
-		int count = 0;
-		
-		for(int i=0; i<N; i++) {
-			A[i] = sc.nextInt();
-		}
-		
-		for(int i=0; i<N; i++) {
-			
-			if(A[i]>=now) {
-				count++;
-				now=A[i];
-			}
-		}
-		
-		System.out.println(count);
-		
-	}
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int dp[] = new int[n];
+        int a[] = new int[n];
+        
+        for (int i = 0; i < n; i++){
+        	
+            a[i] = sc.nextInt();
+            
+            dp[i] = 1;
+            
+            for (int j = 0; j < i; j++){
+            	
+                if (a[j] < a[i] && dp[i] < dp[j] + 1) {
+                	
+                	dp[i] = dp[j]+1;
+                	
+                }
+            }
+        }
+        
+        int max = 0;
+        
+        for (int i = 0; i < n; i++){
+        	
+            if (dp[i] > max){
+                max = dp[i];
+            }
+            
+        }
+        
+        System.out.println(max);
+    }
 	
-
 }
